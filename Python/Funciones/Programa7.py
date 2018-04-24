2dac2f25315da03a075039c89a905c327b96fad1
AlicePeach <alicepeachmelocoton2@gmail.com>
2018-04-24 13:25:38 -0500

    comit

--- Python/Funciones/Programa7.py
+++
@@ -1,7 +0,0 @@
-class Elemento():
-    def __init__(self,lista):
-        for elemento in lista:
-                print('Elemento:',elemento, 'Tipo:', type(elemento))
-def crearObjeto():
-	lista=[1452,11.23,True,'w3sorce',[5,12]]
-	Elemento(lista)
--- Python/Objetos/Programa7.py
+++
@@ -1,5 +0,0 @@
-class Elemento():
-    def __init__(self,lista):
-        for elemento in lista:
-                print('Elemento:',elemento, 'Tipo:', type(elemento))
-lista=[1452,11.23,1+2j,True, 'w3resource',(0,-1),[5,12]]
